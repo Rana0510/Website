baa1f6adc2213f1381ba72e6e76269498a626a1b
Marco Gabriele Ranalli <marco.ranalli@reti.it>
2021-02-25 09:21:23 +0100

    serviceWorkerContainer

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
-      navigator.serviceWorker
+      navigator.serviceWorkerContainer
         .register("/serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err));
